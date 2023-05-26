bc5958d3bb3134fd498eaeee05b8edf9ec618b13
North-West-Wind <wsyn148@gmail.com>
2023-05-26 23:30:37 +0800

    rm lib

--- scripts/splatcur-editor/lib/helper.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function twoDigits(num: number | string): string;
-export declare function decode(path: string): Promise<number[]>;
-export declare function execute(command: string, args: string[], verbose?: boolean): Promise<void>;
